c383a0a8894607061c0e0d28472df9157141da7b
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-10-12 21:08:38 +0300

    Part 1 unicafe step 1.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,17 +2,35 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <h1>Statistics</h1>
+      <Display value={good} text="Good" />
+      <Display value={neutral} text="Neutral" />
+      <Display value={bad} text="Bad" />
     </div>
   )
 }
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+)
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
