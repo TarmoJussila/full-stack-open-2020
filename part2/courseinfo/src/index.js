f11c9bc062caf6abb48240b38adcc564b1868c59
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-11-10 21:19:31 +0200

    Part 2.1: course info step 6.

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -4,27 +4,39 @@ import ReactDOM from 'react-dom'
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
     <>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {/*<Total parts={course.parts} />*/}
     </>
   )
 }
@@ -39,12 +51,12 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      {parts.map(part =>
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      )}
     </>
   )
 }
