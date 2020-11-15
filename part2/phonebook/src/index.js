c627c3e5184ffee48e69a2f4b566a551de671ac5
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-11-15 21:29:15 +0200

    Part 2.6: phonebook fix.

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,27 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-
-}
-
-export default App
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
