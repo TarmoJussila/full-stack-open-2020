5d0bb33e8702808260a40f952c1421cd47a75387
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-11-15 22:14:03 +0200

    Part 2.6: phonebook step 1.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,22 +6,41 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          Name: <input />
+          Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <p key={person.name}>
+          {person.name}
+        </p>
+      )}
     </div>
   )
-
 }
 
 export default App
