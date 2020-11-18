3d7b99c6682ebb41e9890b881fb9d4f62badeefd
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-11-18 19:10:34 +0200

    Part 2.12: step 0. Country finder/filter.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [ countries, setCountries] = useState([])
+  const [ newFilter, setNewFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  const filteredCountries = newFilter.length === 0
+    ? countries
+    : countries.filter(country => country.name.toLowerCase().includes(newFilter.toLowerCase()))
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Country finder</h2>
+      <Filter filterValue={newFilter} onFilterChange={handleFilterChange} />
+      <h3>Country list</h3>
+      <CountryList countries={filteredCountries} />
     </div>
-  );
+  )
 }
 
-export default App;
+const CountryList = ({ countries }) => {
+  return (
+    <div>
+      {countries.map(country =>
+        <p key={country.name}>
+          {country.name} {country.number}
+        </p>
+      )}
+    </div>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      Find countries: <input value={props.filterValue} onChange={props.onFilterChange} />
+    </div>
+  )
+}
+
+export default App
