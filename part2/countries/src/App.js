f0a47504054d0c16b9db1dbb2b65cbdc23618422
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-11-20 15:54:08 +0200

    Part 2.14: countries step 3.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY
+
 const App = () => {
   const [ countries, setCountries ] = useState([])
   const [ newFilter, setNewFilter ] = useState('')
+  const [ currentCountryInfo, setCountryInfo ] = useState({ countryName: '', cityName: '' })
+  const [ cityWeather, setCityWeather ] = useState([])
 
   useEffect(() => {
-    console.log('effect')
+    console.log('country effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
+        console.log('country promise fulfilled')
         setCountries(response.data)
       })
   }, [])
 
+  useEffect(() => {
+    if (currentCountryInfo.cityName === '' || weatherApiKey === '') {
+      return
+    }
+    console.log('weather effect')
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${currentCountryInfo.cityName}&appid=${weatherApiKey}`)
+      .then(response => {
+        console.log('weather promise fulfilled')
+        setCityWeather(response.data)
+      })
+  }, [currentCountryInfo])
+
   const handleFilterChange = (event) => {
     console.log(event.target.value)
     setNewFilter(event.target.value)
+  }
+
+  const handleCountryInfoChange = (countryInfo) => {
+    console.log(countryInfo)
+    setCountryInfo(countryInfo)
+    setNewFilter(countryInfo.countryName)
   }
 
   const filteredCountries = newFilter.length === 0
@@ -28,12 +51,12 @@ const App = () => {
     <div>
       <h2>Country finder</h2>
       <Filter filterValue={newFilter} onFilterChange={handleFilterChange} />
-      <CountryList countries={filteredCountries} onShowDetail={handleFilterChange} />
+      <CountryList countries={filteredCountries} cityWeather={cityWeather} onFilterChange={handleFilterChange} onShowWeather={handleCountryInfoChange} />
     </div>
   )
 }
 
-const CountryList = ({ countries, onShowDetail }) => {
+const CountryList = ({ countries, cityWeather, onFilterChange, onShowWeather }) => {
   if (countries.length > 10) {
     return (
       <div>
@@ -46,7 +69,7 @@ const CountryList = ({ countries, onShowDetail }) => {
     return (
       <div>
         {countries.map(country =>
-          <CountryDetail key={country.name} country={country} />
+          <CountryDetail key={country.name} country={country} cityWeather={cityWeather} onShowWeather={onShowWeather} />
         )}
       </div>
     )
@@ -55,23 +78,32 @@ const CountryList = ({ countries, onShowDetail }) => {
   return (
     <div>
       {countries.map(country =>
-        <Country key={country.name} country={country} onShowDetail={onShowDetail} />
+        <Country key={country.name} country={country} onFilterChange={onFilterChange} />
       )}
     </div>
   )
 }
 
-const Country = ({ country, onShowDetail }) => {
+const Country = ({ country, onFilterChange }) => {
   return (
     <div>
       {country.name}
       {' '}
-      <button type="submit" value={country.name} onClick={onShowDetail}>Show</button>
+      <button type="submit" value={country.name} onClick={onFilterChange}>Show</button>
     </div>
   )
 }
 
-const CountryDetail = ({ country }) => {
+const CountryDetail = ({ country, cityWeather, onShowWeather }) => {
+  const countryInfo = {
+    countryName: country.name,
+    cityName: country.capital
+  }
+
+  const handleShowWeather = () => {
+    onShowWeather(countryInfo);
+  }
+
   return (
     <div>
       <h3>{country.name}</h3>
@@ -84,7 +116,32 @@ const CountryDetail = ({ country }) => {
         </li>
       )}
       <br />
-      <img src={country.flag} alt={country.name} width="100" />
+      <img src={country.flag} alt={country.name} width="100" /><br /><br />
+      <button type="submit" value={country.name} onClick={handleShowWeather}>Update weather</button>
+      <Weather cityWeather={cityWeather} />
+    </div>
+  )
+}
+
+const Weather = ({ cityWeather }) => {
+  if (cityWeather.length === 0) {
+    return <p>No weather info</p>
+  }
+
+  const weatherInfo = {
+    cityName: cityWeather.name,
+    description: cityWeather.weather[0].description,
+    temperature: cityWeather.main.temp,
+    windSpeed: cityWeather.wind.speed,
+    windDegree: cityWeather.wind.deg
+  }
+
+  return (
+    <div>
+      <h4>Weather in {weatherInfo.cityName}</h4>
+      <b>Description:</b> {weatherInfo.description}<br />
+      <b>Temperature:</b> {weatherInfo.temperature} K<br />
+      <b>Wind:</b> {weatherInfo.windSpeed} m/s ({weatherInfo.windDegree} degrees)
     </div>
   )
 }
